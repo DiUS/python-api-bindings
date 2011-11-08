531ef29361784ad7d8730b81a3ef74f4c1913009
Tal Rotbart <redbeard@gmail.com>
2011-11-09 10:19:10 +1100

    Removed unneeded comments

--- springsense_api/meaning_recognition_api.py
+++ springsense_api/meaning_recognition_api.py
@@ -3,14 +3,6 @@ from urllib import quote
 
 from disambiguation_result import DisambiguationResult
 
-# 
-# base_url = "http://172.31.1.235:8080"
-# 
-# conn = Connection(base_url)
-# result = conn.request_post("/disambiguate", args={}, body="cat vet", headers={'Accept':'text/json'})
-# 
-# json.loads(result["body"])
-
 class MeaningRecognitionAPI(object):
 	
 	def __init__(self, url, quote=True):
